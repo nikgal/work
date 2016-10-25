ca4873f9ea8941eab49c6451ece6b5ba3250440c
nikgal <nikgal00@gmail.com>
2016-10-25 17:32:55 +0200

    class Log was created

---
+++ WorkLog/src/by/galov/work/Log.java
@@ -0,0 +1,24 @@
+package by.galov.work;
+
+import java.util.Calendar;
+
+public class Log {
+	private Task currentTask;
+	private Calendar currentDate;
+	public Log(Task currentTask, Calendar currentDate){
+		this.setCurrentTask(currentTask);
+		this.setCurrentDate(currentDate);
+	}
+	public Task getCurrentTask() {
+		return currentTask;
+	}
+	public void setCurrentTask(Task currentTask) {
+		this.currentTask = currentTask;
+	}
+	public Calendar getCurrentDate() {
+		return currentDate;
+	}
+	public void setCurrentDate(Calendar currentDate) {
+		this.currentDate = currentDate;
+	}
+}
