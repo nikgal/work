0cd91df7f347b20c6e30174986ca9e17728d3926
nikgal <nikgal00@gmail.com>
2016-10-25 16:33:56 +0200

    class Tool was updated

--- WorkLog/src/by/galov/work/Tool.java
+++ WorkLog/src/by/galov/work/Tool.java
@@ -1,5 +1,14 @@
 package by.galov.work;
 
 public class Tool {
+	private String toolName;
 
+	public String getToolName() {
+		return toolName;
+	}
+
+	public void setToolName(String toolName) {
+		this.toolName = toolName;
+	}
+	
 }
