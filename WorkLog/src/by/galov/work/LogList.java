292c94266738a07a739b4ba170d7c9ae60748d07
nikgal <nikgal00@gmail.com>
2016-10-25 18:36:25 +0200

    producing filewriting in class LogList

--- WorkLog/src/by/galov/work/LogList.java
+++ WorkLog/src/by/galov/work/LogList.java
@@ -1,6 +1,9 @@
 package by.galov.work;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LogList {
 	private static ArrayList<Log> logList = new ArrayList<>();
@@ -11,6 +14,15 @@ public class LogList {
 		for(Log lg:logList){
 			System.out.println(lg.getCurrentTask().getCurrentTool().getToolName());
 			
+		}
+	}
+	public static void fileLog(Log lg){
+		File f = new File("loglist.txt");
+		try {
+			Scanner fscanner = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
