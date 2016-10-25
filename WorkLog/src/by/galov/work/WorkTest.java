a12c52bb9a66ccf88d6338fddfe8cb68f667da4e
nikgal <nikgal00@gmail.com>
2016-10-25 16:08:26 +0200

    create class Employee

---
+++ WorkLog/src/by/galov/work/WorkTest.java
@@ -0,0 +1,10 @@
+package by.galov.work;
+
+public class WorkTest {
+
+	public static void main(String[] args) {
+		Employee current = new Employee("Nick");
+		System.out.println(current.getName());
+	}
+
+}
